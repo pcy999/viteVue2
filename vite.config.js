0dce2122d609e692b6f7d890c1456e5f748ce203
潘涔洋 <pancenyang@apexsoft.com.cn>
2022-08-09 15:48:27 +0800

    config配置

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,25 @@ import { createVuePlugin } from "vite-plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [createVuePlugin()],
   resolve: {
     alias: {
       "@": join(__dirname, "src"),
     },
   },
-  plugins: [createVuePlugin()],
+  css: {
+    //css预处理
+    preprocessorOptions: {
+      scss: {
+        /*
+				引入var.scss全局预定义变量，
+				如果引入多个文件，
+				可以使用
+				'@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
+				这种格式
+				 */
+        // additionalData: '@import "@/assets/scss/globalVariable.scss";',
+      },
+    },
+  },
 });
